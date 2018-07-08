496ba43f96b2847721f12de192a034829dc28c53
Kriscotiano <christian.o-10@hotmail.com>
2018-07-08 13:25:02 -0500

    Fixed minor issues with hero & added animations to home page.

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,4 @@
+//Nav Scroll Events
 $(window).on('scroll', function() {
     if ($(window).scrollTop()) {
         $('nav').addClass('nav-js');
@@ -22,8 +23,37 @@ $(window).on('scroll', function() {
     }
 });
 
+//Hamburger Events
 $(document).ready(function() {
     $('.toggle-hamburger').on('click', function() {
         $('.main-nav ul').toggleClass('hamburger-active');
     })
+});
+
+
+//Hero Text Event
+$(document).ready(function() {
+    function fadeSpan() {
+        $('#header .hero-box h1 span').fadeIn(2000);
+    }
+    setTimeout(fadeSpan, 6000); 
+});
+
+
+//Section 1 & 2 Events
+$(document).ready(function() {
+    $(window).scroll(function() {
+        let positionTop = $(document).scrollTop();
+        console.log(positionTop);
+
+        if ((positionTop > 500) && (positionTop < 1200)) {
+            $('.main-wrapper .section1 .section1-box .hide').removeClass('hide');
+            $('.main-wrapper .section1 .section1-box .flex-item').addClass('animated fadeInRightBig');
+        }
+
+        if ((positionTop > 1150) && (positionTop < 1500)) {
+            $('.main-wrapper .section2 .section2-box .hide').removeClass('hide');
+            $('.main-wrapper .section2 .section2-box').addClass('animated bounceInDown');
+        }
+    });
 });
